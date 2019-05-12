8ab387048f21f7dbe86d65048c8804841e10cf4c
Kostas Poimenidis <it1530@uom.edu.gr>
2019-05-12 21:34:02 +0300

    This is my Portfolio. It was made with html and css. Also, i used python to create its web server.

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return  render_template('portfolio.html')
+
+@app.route("/Google_Play")
+def Google_Play():
+    return  render_template('Google_Play.html')
+
+@app.route("/Form")
+def Form():
+    return  render_template('Form.html')
+
+if __name__ == '__main__':
+    app.run(debug= True, host="0.0.0.0", port=80)
